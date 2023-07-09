b6f83bd2c9a0c2d611fb82c317fa056902573dec
Pchel <golovkov_1994@mail.ru>
2023-07-10 00:53:49 +0300

    выполнено домашнее задание 17,1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,18 +1,18 @@
 -- SQL-команды для создания таблиц
 create table employees
 (
-	employees_id int primary key,
+	employee_id int primary key,
 	first_name varchar(100) not null,
 	last_name varchar(100) not null,
 	title varchar(100) not null,
-	birth_date int,
-	notes varchar(100) not null
+	birth_date date not null,
+	notes text
 );
 
 
 create table customers
 (
-	customers_id int primary key,
+	customers_id char(5) primary key,
 	company_name varchar (50) not null,
 	contact_name varchar (50) not null
 );
@@ -22,7 +22,7 @@ select * from orders;
 create table orders
 (
 	order_id int primary key,
-	customers_id varchar (50) references customers(custumer_id)  not null,
+	customers_id char (5) references customers(customers_id)  not null,
 	employee_id int references employees(employee_id) not null,
 	order_date date not null,
 	ship_city varchar (100) not null
